990c2f010d7a9e7eb17e5190aee5564c086d9f92
Phil Price <philprice@gmail.com>
2020-03-14 15:31:51 -0700

    Add additional string utils

--- src/grammar/string-utils.ts
+++ src/grammar/string-utils.ts
@@ -21,3 +21,31 @@ export function previous_index(str: string, char: number, start: number) {
 
   return -1;
 }
+
+export function get_next_previous_index(str: string, char: number, index: number) {
+  return [previous_index(str, char, index), next_index(str, char, index)];
+}
+
+export function previous_indices(str: string, char: number, start: number, max: number = Number.MAX_SAFE_INTEGER): number[] {
+  let current = start;
+  let items = [];
+  while (items.length < max && current >= 0) {
+    current = previous_index(str, char, current);
+    items.push(current);
+    current--;
+  }
+
+  return items;
+}
+
+export function next_indices(str: string, char: number, start: number, max: number = Number.MAX_SAFE_INTEGER): number[] {
+  let current = start;
+  let items = [];
+  while (items.length < max && current >= 0) {
+    current = next_index(str, char, current);
+    items.push(current);
+    current++;
+  }
+
+  return items;
+}
